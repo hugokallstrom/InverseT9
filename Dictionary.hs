439c1e31e0378a36363a0c4632189e1868a5634f
hugokallstrom <hugokallstrom@gmail.com>
2014-03-05 12:49:40 +0100

    Comments

---
+++ Dictionary.hs
@@ -0,0 +1,39 @@
+module Dictionary
+( dictionary
+, letterKeys
+) where
+
+dictionary = 
+    [("RICHNESS", 1), ("HAS", 1), ("ETERNAL", 1), ("VALUE", 1), ("IF", 2),
+     ("WE", 1), ("USE", 1), ("IT", 3), ("TO", 13), ("MAKE", 1), ("OTHERS", 2),
+     ("HAPPY", 1), ("MONDAY", 1), ("IS", 9), ("ONE", 1), ("SEVENTH", 1),
+     ("OF", 4), ("YOUR", 4), ("LIFE", 2), ("ALL", 2), ("COMPUTERS", 1),
+     ("WAIT", 1), ("AT", 4), ("THE", 7), ("SAME", 2), ("SPEED", 1), ("BE", 4),
+     ("YOURSELF", 2), ("THERE", 2), ("ARE", 4), ("ENOUGH", 1), ("OTHER", 2),
+     ("PEOPLE", 3), ("A", 4), ("LAUGH", 1), ("SHORTEST", 2), ("DISTANCE", 1),
+     ("BETWEEN", 1), ("TWO", 1), ("WHAT", 3), ("HAPPENS", 1), ("WHEN", 1),
+     ("YOU", 5), ("PLAN", 1), ("DO", 6), ("THINGS", 2), ("I", 2),
+     ("INTEND", 1), ("LIVE", 1), ("FOREVER", 1), ("AND", 2), ("SO", 1),
+     ("FAR", 1), ("EVERYTHING", 2), ("GOES", 1), ("WELL", 3), ("TIME", 3),
+     ("SOMETHING", 2), ("THAT", 2), ("MAKES", 1), ("SURE", 1), ("DOES", 1),
+     ("NOT", 4), ("HAPPEN", 1), ("MOMENT", 1), ("BETTER", 1), ("FAIR", 1),
+     ("THAN", 1), ("POPULAR", 1), ("EVERY", 1), ("WANTED", 1), ("HAVE", 3),
+     ("THOSE", 2), ("WHO", 2), ("GOALS", 1), ("DOOMED", 1), ("WORK", 2),
+     ("FOR", 3), ("STAND", 1), ("TRUTH", 1), ("EVEN", 1), ("EXPENSE", 1),
+     ("LOSING", 1), ("POPULARITY", 1), ("WANT", 1), ("THINK", 1), ("SPEAK", 1),
+     ("OUR", 1), ("ERRORS", 1), ("STEPS", 1), ("ON", 1), ("WAY", 3),
+     ("LOYALTY", 2), ("FRIENDS", 1), ("CERTAIN", 1), ("BASIC", 1),
+     ("PRINCIPLES", 1), ("ESSENTIAL", 1), ("SUCCESS", 2), ("IN", 1),
+     ("ANY", 1), ("SPHERE", 1), ("NOTHING", 1), ("BAD", 1), ("BUT", 3),
+     ("MIGHT", 1), ("BEEN", 1), ("WORSE", 1), ("MANAGE", 1), ("DETERMINES", 1),
+     ("OR", 1), ("FAILURE", 1), ("AS", 1), ("HUMAN", 1), ("LEARN", 1),
+     ("NEW", 1), ("EACH", 1), ("DAY", 1), ("FORGET", 2), ("INJURIES", 1),
+     ("NEVER", 1), ("KINDNESS", 1), ("UNDERTAKE", 1), ("CANNOT", 1),
+     ("PERFORM", 1), ("CAREFUL", 1), ("KEEP", 1), ("PROMISE", 1), ("MANY", 1),
+     ("SINGLE", 1), ("THING", 1), ("NO", 1), ("SUBSTITUTE", 1), ("HARD", 1)]
+
+letterKeys = 
+    [('A', 2), ('B', 2), ('C', 2), ('D', 3), ('E', 3), ('F', 3), ('G', 4), ('H', 4),
+     ('I', 4), ('J', 5), ('K', 5), ('L', 5), ('M', 6), ('N', 6), ('O', 6), ('P', 7),
+     ('Q', 7), ('R', 7), ('S', 7), ('T', 8), ('U', 8), ('V', 8), ('W', 9), ('X', 9),
+     ('Y', 9), ('Z', 9)] 
--- t9.hs
+++ t9.hs
@@ -1,3 +1,11 @@
+{- Author: Hugo Källström 
+ - Date: 4/5 - 2014
+ - 
+ - Inverse T9 
+ - Takes a message as input and returns the least
+ - keypresses possible to write the message on 
+ - a phone. 
+ -}
 import qualified Data.List as List
 import Data.Char
 import Data.Ord
@@ -5,37 +13,55 @@ import Messages
 import Data.Maybe
 import Dictionary
 
+-- Takes a string as input and returns a list
+-- with the words in the string.
 getWords :: [Char] -> [[Char]]
 getWords message = (head words):map tail (tail words)
     where words = List.groupBy (\x y -> y /= ' ') message
 
+-- Converts a list of words to a list of ints 
+-- which is the numeric representation of the words.
 processWords :: [[Char]] -> [[Int]]
 processWords [] = []
 processWords (word:rest) = replace word:processWords rest 
 
+-- Converts all the words in the dictionary 
+-- to their respective numeric representation.
 convertDict :: [([Char], Integer)] -> [([Int], Int)]
 convertDict [] = []
 convertDict (tuple:rest) = (replace $ fst tuple, fromIntegral $ snd tuple):convertDict rest
 
+-- Converts a word to its repsective numeric 
+-- representation.
 replace :: [Char] -> [Int]
 replace [] = []
 replace (letter:rest) = (fromIntegral $ snd $ head $ filter (\(x,y) -> x == letter) letterKeys):replace rest
 
+-- Takes several messages as input and 
+-- returns a list of strings which represent 
+-- keypresses on a numeric keyboard.
 compute :: [[Char]] -> [[Char]]
 compute [] = []
 compute (firstMess:rest) = (init $ computeMessage numMess words):compute rest
 	where numMess = processWords $ getWords firstMess
 	      words = getWords firstMess
 
+-- Takes a list of words in both numeric and alphabetical
+-- representation as input. Checks if the word 
+-- is in the dictionary, else calls findKeyPresses.
 computeMessage :: [[Int]] -> [[Char]] -> [Char]
 computeMessage [] _ = []
 computeMessage (firstNumMess:numMess) (firstMess:mess)
 	| filter (\(x,y) -> x == firstMess) dictionary == []    = ['?'] ++ ['0'] ++ (computeMessage numMess mess)
 	| otherwise = (compareLength (findKeyPresses firstNumMess firstMess)) ++ ['0'] ++ (computeMessage numMess mess)
 
+-- Takes a word as input and returns its rang in the dicitionary.
 getRang :: [Char] -> Int
 getRang word = fromIntegral $ snd $ fromJust $ List.find (\(x,y) -> x == word) dictionary
 
+-- Takes a word in both numerical and alphabetical representation 
+-- and calculates the number of modifications to write the word
+-- with 1 to n characters. Where n is the length of the word. 
 findKeyPresses :: [Int] -> [Char] -> [[Char]] 
 findKeyPresses [] _ = []
 findKeyPresses numWord charWord = (toString numWord ++ (take (rangCompare + dictCompare) $ repeat '^'))
@@ -43,6 +69,8 @@ findKeyPresses numWord charWord = (toString numWord ++ (take (rangCompare + dict
 	where rangCompare = length $ filter (\(x,y) -> y > getRang charWord) (searchDict numWord)
       	      dictCompare = fromIntegral $ sum $ lexCompare numWord charWord (filter (\(x,y) -> y == getRang charWord) (searchDict numWord))
 
+-- Calculates the number of words which has a greater 
+-- alphabetical ordering.
 lexCompare :: [Int] -> [Char] -> [([Int], Int)] -> [Integer]
 lexCompare _ _ [] = []
 lexCompare numWord word (first:rest) 
@@ -51,6 +79,8 @@ lexCompare numWord word (first:rest)
 	| (compare word dictWord == EQ)    = 0:lexCompare numWord word rest
 	where dictWord = fst $ dictionary !! (head $ (List.elemIndices first $ convertDict dictionary))
 
+-- Finds the smallest word in the input list.
+-- Prioritizes '^' over a letter.
 compareLength :: [[Char]] -> [Char]
 compareLength keys 
 	| length minKeys == 1           = head minKeys
@@ -58,10 +88,13 @@ compareLength keys
 	| otherwise = compareLength $ tail minKeys 
 	where minKeys = filter (\x -> length x <= length (List.minimumBy (comparing length) keys)) keys
 
+-- Searches the dictionary for words starting 
+-- with the same numbers as the input word.
 searchDict :: [Int] -> [([Int], Int)]
 searchDict numWord = filter (\(x, _) -> numWord `List.isPrefixOf` x) dict
 	where dict = convertDict dictionary
 
+-- Converts a list of ints to a list of chars.
 toString :: [Int] -> [Char]
 toString [] = []
 toString (int:rest) = intToDigit int:toString rest
